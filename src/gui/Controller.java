a54376716991438e050fce912495ff3f590637b3
Mikolaj Deja <dejamikolaj@gmail.com>
2021-05-05 22:36:35 +0200

    implemented a dirty take on user input

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -8,8 +8,6 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceDialog;
 import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
